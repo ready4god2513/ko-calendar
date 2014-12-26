fef86e50d2ba591a107648ada4a39e39db67785e
Jacob Kelley <jakie8@gmail.com>
2014-12-26 09:18:00 -0800

    Lots of style additions

--- src/js/ko-calendar.js
+++ src/js/ko-calendar.js
@@ -1,5 +1,8 @@
-ko.components.register('calendar', {
-	viewModel: function(params) {
+(function() {
+
+	var binding = 'calendar';
+
+	var Model = function(params) {
 		var self = this;
 
 		self.strings = {
@@ -8,7 +11,8 @@ ko.components.register('calendar', {
 		};
 
 		self.constants = {
-			daysInWeek: 7
+			daysInWeek: 7,
+			dayStringLength: 2
 		};
 
 		self.utils = {
@@ -100,28 +104,43 @@ ko.components.register('calendar', {
 
 			return weeks;
 		});
-	},
-	template:	'<div class="ko-calendar" data-bind="with: $data">\
-					<header>\
-						<b data-bind="text: strings.months[current().getMonth()] + \' \' + current().getFullYear()"></b>\
-						<span class="pull-right">\
-							<a href="#" data-bind="click: prevMonth">&larr;</a>\
-							<a href="#" data-bind="click: nextMonth">&rarr;</a>\
-						</span>\
-					</header>\
-					<table>\
-						<thead>\
-							<tr data-bind="foreach: strings.days">\
-								<th data-bind="text: $data"></th>\
-							</tr>\
-						</thead>\
-						<tbody data-bind="foreach: sheet">\
-							<tr class="week" data-bind="foreach: $data">\
-								<td class="day" data-bind="css: { weekend: $parents[1].utils.date.isWeekend($data), today: $parents[1].utils.date.isSame(new Date(), $data) } ">\
-									<a href="javascript:;" data-bind="text: $data.getDate(), attr: { title: $data }, click: $parents[1].select, css: { active: $parents[1].utils.date.isSame($parents[1].selected(), $data) } "></a>\
-								</td>\
-							</tr>\
-						</tbody>\
-					</table>\
-				</div>'
-});
+	};
+
+	var Template =
+		'<div class="ko-calendar" data-bind="with: $data">\
+			<table>\
+				<thead>\
+					<tr>\
+						<th>\
+							<a href="#" data-bind="click: prevMonth" class="prev">&laquo;</a>\
+						</th>\
+						<th colspan="5">\
+							<b data-bind="text: strings.months[current().getMonth()] + \' \' + current().getFullYear()"></b>\
+						</th>\
+						<th>\
+							<a href="#" data-bind="click: nextMonth" class="next">&raquo;</a>\
+						</th>\
+					</tr>\
+					<tr data-bind="foreach: strings.days">\
+						<th data-bind="text: $data.substring(0, $parents[1].constants.dayStringLength)"></th>\
+					</tr>\
+				</thead>\
+				<tbody data-bind="foreach: sheet">\
+					<tr class="week" data-bind="foreach: $data">\
+						<td class="day" data-bind="css: { weekend: $parents[1].utils.date.isWeekend($data), today: $parents[1].utils.date.isSame(new Date(), $data) } ">\
+							<a href="javascript:;" data-bind="text: $data.getDate(), attr: { title: $data }, click: $parents[1].select, css: { active: $parents[1].utils.date.isSame($parents[1].selected(), $data) } "></a>\
+						</td>\
+					</tr>\
+				</tbody>\
+			</table>\
+		</div>';
+
+
+	// Component
+	ko.components.register(binding, {
+		viewModel: Model,
+		template: Template
+	});
+
+
+}).call(this);
