80e79bbd8f6a9bc482364fa138bd59aadc5b0eb3
Jacob Kelley <jakie8@gmail.com>
2014-12-25 18:34:41 -0800

    Renamed some strings and comments

--- src/js/ko-calendar.js
+++ src/js/ko-calendar.js
@@ -3,8 +3,8 @@ ko.components.register('calendar', {
 		var self = this;
 
 		self.strings = {
-			months: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" ],
-			days: [ "Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat" ]
+			months: [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "Octover", "November", "December" ],
+			days: [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
 		};
 
 		self.constants = {
@@ -72,53 +72,56 @@ ko.components.register('calendar', {
 		    while(true) {
 		    	if(!weeks[week]) { weeks[week] = []; }
 
+		    	// If we haven't filled the current week up
 		    	if(weeks[week].length !== self.constants.daysInWeek) {
+
+		    		// Append to the week
 		    		weeks[week].push(new Date(normalized.getTime()));
+
+		    		// And increment the date
 		    		normalized.setDate( normalized.getDate() + 1 );
 		    	}
 
 		    	// If we've began working within the current month
 		    	if( normalized.getMonth() == self.current().getMonth() ) { startedMonth = true; }
 
-		    	// If we've changed months and we've started our current month (and thus completed it)
+		    	// If we've started our current month and we've changed months (and thus completed it)
 		    	if( startedMonth && (normalized.getMonth() !== self.current().getMonth()) ) { completedMonth = true; }
 
 		    	// If we've completed our month and we are at the end of the week
 		    	if(completedMonth && weeks[week].length == self.constants.daysInWeek) { completedWeek = true; }
 
-		    	// If we've completed the month and our week is finished
+		    	// If we've completed the month and our week
 		    	if(completedMonth && completedWeek) { break; }
 
-		    	// If we're at the end of the week, increment the current week
+		    	// Otherwise, if we're at the end of the week, increment the current week
 		    	if(weeks[week].length == self.constants.daysInWeek) { week++; }
 		    }
 
 		    return weeks;
 		});
     },
-    template:
-    	'<div class="ko-calendar" data-bind="with: $data">\
-			<header>\
-				<b data-bind="text: strings.months[current().getMonth()] + \' \' + current().getFullYear()"></b>\
-				<span class="pull-right">\
-					<a href="#" data-bind="click: prevMonth">&larr;</a>\
-					<a href="#" data-bind="click: nextMonth">&rarr;</a>\
-				</span>\
-			</header>\
-			<table>\
-				<thead>\
-					<tr data-bind="foreach: strings.days">\
-						<th data-bind="text: $data"></th>\
-					</tr>\
-				</thead>\
-				<tbody data-bind="foreach: sheet">\
-					<tr class="week" data-bind="foreach: $data">\
-						<td class="day" data-bind="css: { weekend: $parents[1].utils.date.isWeekend($data), today: $parents[1].utils.date.isSame(new Date(), $data) } ">\
-							<a href="javascript:;" data-bind="text: $data.getDate(), attr: { title: $data }, click: $parents[1].select, css: { active: $parents[1].utils.date.isSame($parents[1].selected(), $data) } "></a>\
-						</td>\
-					</tr>\
-				</tbody>\
-			</table>\
-		</div>'
-
+    template:	'<div class="ko-calendar" data-bind="with: $data">\
+					<header>\
+						<b data-bind="text: strings.months[current().getMonth()] + \' \' + current().getFullYear()"></b>\
+						<span class="pull-right">\
+							<a href="#" data-bind="click: prevMonth">&larr;</a>\
+							<a href="#" data-bind="click: nextMonth">&rarr;</a>\
+						</span>\
+					</header>\
+					<table>\
+						<thead>\
+							<tr data-bind="foreach: strings.days">\
+								<th data-bind="text: $data"></th>\
+							</tr>\
+						</thead>\
+						<tbody data-bind="foreach: sheet">\
+							<tr class="week" data-bind="foreach: $data">\
+								<td class="day" data-bind="css: { weekend: $parents[1].utils.date.isWeekend($data), today: $parents[1].utils.date.isSame(new Date(), $data) } ">\
+									<a href="javascript:;" data-bind="text: $data.getDate(), attr: { title: $data }, click: $parents[1].select, css: { active: $parents[1].utils.date.isSame($parents[1].selected(), $data) } "></a>\
+								</td>\
+							</tr>\
+						</tbody>\
+					</table>\
+				</div>'
 });
