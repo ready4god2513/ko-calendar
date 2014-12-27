89d204f6da86dbd13d3591853bf22b8458b288a7
Jacob Kelley <jakie8@gmail.com>
2014-12-26 16:21:10 -0800

    More style additions, added more parameters and started commenting

--- src/js/ko-calendar.js
+++ src/js/ko-calendar.js
@@ -5,6 +5,18 @@
 	var Model = function(params) {
 		var self = this;
 
+		self.opts = {
+			value: ko.observable(),
+			current: ko.observable(),
+			deselectable: false,
+			input: false,
+			inputRenderer: function() {
+				return '<input type="text">';
+			}
+		};
+
+		ko.utils.extend(self.opts, params);
+
 		self.strings = {
 			months: [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
 			days: [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
@@ -17,6 +29,12 @@
 
 		self.utils = {
 			date: {
+
+				/**
+				 * Takes a given date and sets the time to midnight
+				 * @param  {Date} d The date object to normalize
+				 * @return {Date}   The normalized date
+				 */
 				normalize: function(d) {
 					var normalized = new Date(d.getTime());
 					normalized.setMinutes(0);
@@ -25,6 +43,12 @@
 					return normalized;
 				},
 
+				/**
+				 * Checks if two date objects are on the same day
+				 * @param  {Date}  d1 The first date
+				 * @param  {Date}  d2 The second date
+				 * @return {Boolean}    Whether or not the dates share the same day
+				 */
 				isSame: function(d1, d2) {
 					if(!d1 || !d2) { return false; }
 					return (
@@ -34,6 +58,22 @@
 					);
 				},
 
+				/**
+				 * Checks if the two date objects have different months
+				 * @param  {Date}  d1 The first date
+				 * @param  {Date}  d2 The second date
+				 * @return {Boolean}    Whether or not the dates share the same month
+				 */
+				isSameMonth: function(d1, d2) {
+					if(!d1 || !d2) { return false; }
+					return d1.getUTCMonth() == d2.getUTCMonth();
+				},
+
+				/**
+				 * Checks if the given date falls on the weekend (Saturday or Sunday)
+				 * @param  {Date}  d The date to check
+				 * @return {Boolean}   Whether or not the date falls on a weekend
+				 */
 				isWeekend: function(d) {
 					if(!d) { return false; }
 					return d.getUTCDay() === 0 || (d.getUTCDay() == self.constants.daysInWeek - 1);
@@ -42,11 +82,17 @@
 		};
 
 		// Date Alias Helpers
-		self.current = ko.observable(new Date()); // The current Date
+		self.current = ko.observable(self.opts.current || new Date()); // The current Date
 		self.selected = ko.observable(); // The selected Date
+		self.selected.subscribe(self.opts.value);
 
 		// Selects a date
-		self.select = function(data, e) { self.selected(data); };
+		self.select = function(data, e) {
+			if( self.opts.deselectable && self.utils.date.isSame(self.selected(), data) ) {
+				return self.selected(null);
+			}
+			self.selected(data);
+		};
 
 		self.nextMonth = function() {
 			self.current(
@@ -106,33 +152,42 @@
 		});
 	};
 
+	self.focused = ko.observable(false);
+	self.doFocus = function() { self.focused(true); };
+	self.doBlur = function() { self.focused(false); };
+
 	var Template =
 		'<div class="ko-calendar" data-bind="with: $data">\
-			<table>\
-				<thead>\
-					<tr>\
-						<th>\
-							<a href="#" data-bind="click: prevMonth" class="prev">&laquo;</a>\
-						</th>\
-						<th colspan="5">\
-							<b data-bind="text: strings.months[current().getMonth()] + \' \' + current().getFullYear()"></b>\
-						</th>\
-						<th>\
-							<a href="#" data-bind="click: nextMonth" class="next">&raquo;</a>\
-						</th>\
-					</tr>\
-					<tr data-bind="foreach: strings.days">\
-						<th data-bind="text: $data.substring(0, $parents[1].constants.dayStringLength)"></th>\
-					</tr>\
-				</thead>\
-				<tbody data-bind="foreach: sheet">\
-					<tr class="week" data-bind="foreach: $data">\
-						<td class="day" data-bind="css: { weekend: $parents[1].utils.date.isWeekend($data), today: $parents[1].utils.date.isSame(new Date(), $data) } ">\
-							<a href="javascript:;" data-bind="text: $data.getDate(), attr: { title: $data }, click: $parents[1].select, css: { active: $parents[1].utils.date.isSame($parents[1].selected(), $data) } "></a>\
-						</td>\
-					</tr>\
-				</tbody>\
-			</table>\
+			<!-- ko if: opts.input -->\
+			<input type="text" data-bind="event: { focus: doFocus, blur: doBlur }">\
+			<!-- /ko -->\
+			<div class="calendar-sheet" data-bind="visible: focused() || !opts.input">\
+				<table data-bind="css: { selected: selected } ">\
+					<thead>\
+						<tr>\
+							<th>\
+								<a href="#" data-bind="click: prevMonth" class="prev">&laquo;</a>\
+							</th>\
+							<th colspan="5">\
+								<b data-bind="text: strings.months[current().getMonth()] + \' \' + current().getFullYear()"></b>\
+							</th>\
+							<th>\
+								<a href="#" data-bind="click: nextMonth" class="next">&raquo;</a>\
+							</th>\
+						</tr>\
+						<tr data-bind="foreach: strings.days">\
+							<th data-bind="text: $data.substring(0, $parents[1].constants.dayStringLength)"></th>\
+						</tr>\
+					</thead>\
+					<tbody data-bind="foreach: sheet">\
+						<tr class="week" data-bind="foreach: $data">\
+							<td class="day" data-bind="css: { weekend: $parents[1].utils.date.isWeekend($data), today: $parents[1].utils.date.isSame(new Date(), $data), inactive: !($parents[1].utils.date.isSameMonth($parents[1].current(), $data)) } ">\
+								<a href="javascript:;" data-bind="text: $data.getDate(), attr: { title: $data }, click: $parents[1].select, css: { active: $parents[1].utils.date.isSame($parents[1].selected(), $data) } "></a>\
+							</td>\
+						</tr>\
+					</tbody>\
+				</table>\
+				</div>\
 		</div>';
 
 
@@ -142,5 +197,11 @@
 		template: Template
 	});
 
+	// Binding Handler
+	ko.bindingHandlers[binding] = {
+		update: function(el, params) {
+			el.innerHTML = Template;
+		}
+	};
 
 }).call(this);
