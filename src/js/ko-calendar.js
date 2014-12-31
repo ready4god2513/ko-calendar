4c41ad3ddedd7937359130dd16e26a77e6acb14d
Jacob Kelley <jakie8@gmail.com>
2014-12-30 17:05:44 -0800

    Did a ton with the time selector and fixed more UTC bugs. Going to work on AM/PM support

--- src/js/ko-calendar.js
+++ src/js/ko-calendar.js
@@ -7,12 +7,24 @@
 
 		self.opts = {
 			value: ko.observable(),
-			current: ko.observable(),
+			current: new Date(),
+			selected: null,
 			deselectable: true,
-			showToday: true
+
+			showCalendar: true,
+			showToday: true,
+
+			showTime: true,
+			showNow: true,
+			militaryTime: false
 		};
 
 		ko.utils.extend(self.opts, params);
+
+		if(!self.opts.calendar && !self.opts.showTime) {
+			console.error('Silly goose, what are you using ko-calendar for?');
+			return;
+		}
 
 		self.strings = {
 			months: [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
@@ -34,9 +46,7 @@
 				 */
 				normalize: function(d) {
 					var normalized = new Date(d.getTime());
-					normalized.setMinutes(0);
-					normalized.setHours(0);
-					normalized.setSeconds(0);
+					normalized.setUTCHours(0, 0, 0);
 					return normalized;
 				},
 
@@ -75,124 +85,184 @@
 					if(!d) { return false; }
 					return d.getUTCDay() === 0 || (d.getUTCDay() == self.constants.daysInWeek - 1);
 				}
+			},
+			strings: {
+				pad: function(n) {
+					return n < 10 ? "0" + n : n;
+				}
 			}
 		};
 
 		// Date Alias Helpers
 		self.current = ko.observable(self.opts.current || new Date()); // The current Date
-		self.selected = ko.observable(); // The selected Date
+		self.selected = ko.observable(self.opts.selected); // The selected Date
 		self.selected.subscribe(self.opts.value);
+		self.opts.value(self.opts.selected);
 
-		// Selects a date
-		self.select = function(data, e) {
-			console.log(self.opts);
-			if( self.opts.deselectable && self.utils.date.isSame(self.selected(), data) ) {
-				return self.selected(null);
-			}
-			self.selected(data);
-		};
-		self.select_today = function(data, e) {
-			var d = new Date();
-			self.selected(d);
-			self.current(d);
-		};
+		self.calendar = {
 
-		self.nextMonth = function() {
-			self.current(
-				new Date( self.current().setMonth(self.current().getUTCMonth()+1) )
-			);
-		};
+			// Selects a date
+			select: function(data, e) {
+				if( self.opts.deselectable && self.utils.date.isSame(self.selected(), data) ) {
+					return self.selected(null);
+				}
+				self.selected(data);
+			},
+			selectToday: function(data, e) {
+				var d = self.utils.date.normalize(new Date());
+				self.selected(d);
+				self.current(d);
+			},
+			next: function() {
+				self.current(
+					new Date( self.current().setUTCMonth(self.current().getUTCMonth()+1) )
+				);
+			},
+			prev: function() {
+				self.current(
+					new Date( self.current().setUTCMonth(self.current().getUTCMonth()-1) )
+				);
+			},
+			sheet: ko.computed(function() {
 
-		self.prevMonth = function() {
-			self.current(
-				new Date( self.current().setMonth(self.current().getUTCMonth()-1) )
-			);
-		};
+				// Current month set to the first day
+				var normalized = self.utils.date.normalize(self.current());
+				normalized.setUTCDate(1);
+				normalized.setUTCDate(normalized.getUTCDate() - normalized.getUTCDay()); // Set our date to the first day of the week from the normalized month
 
-		self.sheet = ko.computed(function() {
+				var weeks = [];
+				var week = 0;
+				var startedMonth = false;
+				var completedMonth = false;
+				var completedWeek = false;
 
-			// Current month set to the first day
-			var normalized = self.utils.date.normalize(self.current());
-			normalized.setDate(1);
-			normalized.setDate(normalized.getUTCDate() - normalized.getUTCDay()); // Set our date to the first day of the week from the normalized month
+				while(true) {
+					if(!weeks[week]) { weeks[week] = []; }
 
-			var weeks = [];
-			var week = 0;
-			var startedMonth = false;
-			var completedMonth = false;
-			var completedWeek = false;
+					// If we haven't filled the current week up
+					if(weeks[week].length !== self.constants.daysInWeek) {
 
-			while(true) {
-				if(!weeks[week]) { weeks[week] = []; }
+						// Append to the week
+						weeks[week].push(new Date(normalized.getTime()));
 
-				// If we haven't filled the current week up
-				if(weeks[week].length !== self.constants.daysInWeek) {
+						// And increment the date
+						normalized.setUTCDate( normalized.getUTCDate() + 1 );
+					}
 
-					// Append to the week
-					weeks[week].push(new Date(normalized.getTime()));
+					// If we've began working within the current month
+					if( normalized.getUTCMonth() == self.current().getUTCMonth() ) { startedMonth = true; }
 
-					// And increment the date
-					normalized.setDate( normalized.getUTCDate() + 1 );
+					// If we've started our current month and we've changed months (and thus completed it)
+					if( startedMonth && (normalized.getUTCMonth() !== self.current().getUTCMonth()) ) { completedMonth = true; }
+
+					// If we've completed our month and we are at the end of the week
+					if(completedMonth && weeks[week].length == self.constants.daysInWeek) { completedWeek = true; }
+
+					// If we've completed the month and our week
+					if(completedMonth && completedWeek) { break; }
+
+					// Otherwise, if we're at the end of the week, increment the current week
+					if(weeks[week].length == self.constants.daysInWeek) { week++; }
 				}
 
-				// If we've began working within the current month
-				if( normalized.getUTCMonth() == self.current().getUTCMonth() ) { startedMonth = true; }
+				return weeks;
+			})
+		};
 
-				// If we've started our current month and we've changed months (and thus completed it)
-				if( startedMonth && (normalized.getUTCMonth() !== self.current().getUTCMonth()) ) { completedMonth = true; }
-
-				// If we've completed our month and we are at the end of the week
-				if(completedMonth && weeks[week].length == self.constants.daysInWeek) { completedWeek = true; }
-
-				// If we've completed the month and our week
-				if(completedMonth && completedWeek) { break; }
-
-				// Otherwise, if we're at the end of the week, increment the current week
-				if(weeks[week].length == self.constants.daysInWeek) { week++; }
-			}
-
-			return weeks;
-		});
+		self.time = {
+			next: function(data, e) {
+				if(!self.selected()) { return self.time.selectNow(); }
+				self.selected(
+					new Date( self.selected()[data.setter]( self.selected()[data.getter]() + 1 ) )
+				);
+			},
+			prev: function(data, e) {
+				if(!self.selected()) { return self.time.selectNow(); }
+				self.selected(
+					new Date( self.selected()[data.setter]( self.selected()[data.getter]() - 1 ) )
+				);
+			},
+			selectNow: function() {
+				var d = new Date();
+				self.selected(d);
+				self.current(d);
+			},
+			sheet: ko.observableArray([
+				{ getter: 'getUTCHours', setter: 'setUTCHours' },
+				{ getter: 'getUTCMinutes', setter: 'setUTCMinutes' }
+			])
+		};
+		if(self.opts.militaryTime) {
+			self.time.sheet.push({});
+		}
 	};
 
 	var Template =
-		'<div class="ko-calendar" data-bind="with: $data">\
-			<div class="calendar-sheet">\
-				<table data-bind="css: { selected: selected } ">\
-					<thead>\
+		'<div class="ko-calendar" data-bind="with: $data, visible: opts.showCalendar || opts.showTime">\
+			<!-- ko if: opts.showCalendar -->\
+			<table data-bind="css: { selected: selected } " class="calendar-sheet">\
+				<thead>\
+					<tr>\
+						<th>\
+							<a href="#" data-bind="click: calendar.prev" class="prev">&laquo;</a>\
+						</th>\
+						<th data-bind="attr: { colspan: constants.daysInWeek - 2 } ">\
+							<b data-bind="text: strings.months[current().getUTCMonth()] + \' \' + current().getFullYear()"></b>\
+						</th>\
+						<th>\
+							<a href="#" data-bind="click: calendar.next" class="next">&raquo;</a>\
+						</th>\
+					</tr>\
+					<tr data-bind="foreach: strings.days">\
+						<th data-bind="text: $data.substring(0, $parents[1].constants.dayStringLength)"></th>\
+					</tr>\
+				</thead>\
+				<tbody data-bind="foreach: calendar.sheet">\
+					<tr class="week" data-bind="foreach: $data">\
+						<td class="day" data-bind="css: { weekend: $parents[1].utils.date.isWeekend($data), today: $parents[1].utils.date.isSame(new Date(), $data), inactive: !($parents[1].utils.date.isSameMonth($parents[1].current(), $data)) } ">\
+							<a href="javascript:;" data-bind="text: $data.getUTCDate(), attr: { title: $data }, click: $parents[1].calendar.select, css: { active: $parents[1].utils.date.isSame($parents[1].selected(), $data) } "></a>\
+						</td>\
+					</tr>\
+				</tbody>\
+				<!-- ko if: opts.showToday -->\
+					<tfoot>\
 						<tr>\
-							<th>\
-								<a href="#" data-bind="click: prevMonth" class="prev">&laquo;</a>\
-							</th>\
-							<th data-bind="attr: { colspan: constants.daysInWeek - 2 } ">\
-								<b data-bind="text: strings.months[current().getUTCMonth()] + \' \' + current().getFullYear()"></b>\
-							</th>\
-							<th>\
-								<a href="#" data-bind="click: nextMonth" class="next">&raquo;</a>\
-							</th>\
-						</tr>\
-						<tr data-bind="foreach: strings.days">\
-							<th data-bind="text: $data.substring(0, $parents[1].constants.dayStringLength)"></th>\
-						</tr>\
-					</thead>\
-					<tbody data-bind="foreach: sheet">\
-						<tr class="week" data-bind="foreach: $data">\
-							<td class="day" data-bind="css: { weekend: $parents[1].utils.date.isWeekend($data), today: $parents[1].utils.date.isSame(new Date(), $data), inactive: !($parents[1].utils.date.isSameMonth($parents[1].current(), $data)) } ">\
-								<a href="javascript:;" data-bind="text: $data.getUTCDate(), attr: { title: $data }, click: $parents[1].select, css: { active: $parents[1].utils.date.isSame($parents[1].selected(), $data) } "></a>\
+							<td data-bind="attr: { colspan: constants.daysInWeek } ">\
+								<a href="javascript:;" data-bind="click: calendar.selectToday">Today</a>\
 							</td>\
 						</tr>\
-					</tbody>\
-					<!-- ko if: opts.showToday -->\
-						<tfoot>\
-							<tr>\
-								<td data-bind="attr: { colspan: constants.daysInWeek } ">\
-									<a href="javascript:;" data-bind="click: select_today">Today</a>\
-								</td>\
-							</tr>\
-						</tfoot>\
-					<!-- /ko -->\
-				</table>\
-			</div>\
+					</tfoot>\
+				<!-- /ko -->\
+			</table>\
+			<!-- /ko -->\
+			<!-- ko if: opts.showTime -->\
+			<table class="time-sheet">\
+				<tbody>\
+					<tr data-bind="foreach: time.sheet">\
+						<td>\
+							<a href="#" class="up" data-bind="click: $parent.time.next"></a>\
+						</td>\
+					</tr>\
+					<tr data-bind="foreach: time.sheet">\
+						<td data-bind="css: { colon: $index() === 0, inactive: !$parent.selected() }, text: $parent.selected() ? $parent.utils.strings.pad( $parent.selected()[getter]() ) : \'-\'"></td>\
+					</tr>\
+					<tr data-bind="foreach: time.sheet">\
+						<td>\
+							<a href="#" class="down" data-bind="click: $parent.time.prev"></a>\
+						</td>\
+					</tr>\
+				</tbody>\
+				<!-- ko if: opts.showNow -->\
+					<tfoot>\
+						<tr>\
+							<td data-bind="attr: { colspan: time.sheet().length } ">\
+								<a href="javascript:;" data-bind="click: time.selectNow">Now</a>\
+							</td>\
+						</tr>\
+					</tfoot>\
+				<!-- /ko -->\
+			</table>\
+			<!-- /ko -->\
 		</div>';
 
 
@@ -201,12 +271,5 @@
 		viewModel: Model,
 		template: Template
 	});
-
-	// Binding Handler
-	ko.bindingHandlers[binding] = {
-		update: function(el, params) {
-			el.innerHTML = Template;
-		}
-	};
 
 }).call(this);
